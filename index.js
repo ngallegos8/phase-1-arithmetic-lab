c1c03f5670581729ece35cbaa86fe64ebd87dd1a
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-12 08:59:17 -0700

    4 passing tests

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random())
+
+const num3 = 32;
+const num4 = 7;
+const mod = num3 % num4;
+
+
+const max = Math.max(2,4,6,8,10,12,14,16,18,20);
